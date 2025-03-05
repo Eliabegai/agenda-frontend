4d3ed9f33eedfcc1ae4906d1bb13ab4020f87f75
Eliabe Gai <eliabegai@gmail.com>
2025-03-05 08:36:05 -0300

    feat: template

--- src/components/Template/Pagina.tsx
+++ src/components/Template/Pagina.tsx
@@ -1,9 +1,12 @@
 'use client';
 import React, { ReactNode, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faCalendarDays, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Calendar } from '../ui/calendar';
 import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { ModeToggle } from '../ModeToggle/modeToggler';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu';
 
 type PaginaProps = {
   children: ReactNode;
@@ -13,28 +16,72 @@ type PaginaProps = {
 const Pagina = (props: PaginaProps) => {
   const [date, setDate] = useState<Date | undefined>(new Date());
   const [funcionario, setFuncionario] = useState<string>('');
+  
+  const handleClick = () => {
+    if(funcionario)
+      alert(`Buscar Funcionario ${funcionario} no banco de dados.`)
+    return
+  }
+  const handleClickFuncionario = () => {
+    alert(`Gerenciar Funcionarios.`)
+  }
+
+  const gerarFuncionarios = () => {
+    return Array.from({length: 10}, (_, id) => ({
+      id: id+1,
+      nome: `Funcionario ${id + 1}`
+    }))
+  }
+
   return (
     <div className="flex flex-col m-auto px-4 py-2 w-full h-screen">
-      <header className="flex border-b-2 p-5 justify-center items-center">Header</header>
+      <header className="flex border-b-2 px-4 pt-4 pb-1 justify-between items-center">
+        <div className='text-2xl flex items-center space-x-2'>
+          <FontAwesomeIcon icon={faCalendarDays} color='var(--color-cyan-500)' />
+          <h1 className='text-3xl font-bold'>Agenda de Trabalho</h1>
+        </div>
+        <div className='space-x-2'>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <FontAwesomeIcon icon={faUser} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onClick={() => alert('Editar Perfil')}>Editar Perfil</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <ModeToggle />
+        </div>
+        </header>
       <div className="flex flex-row h-screen w-full mt-4 overflow-auto">
-        <aside className="flex flex-col w-72 h-svh border-r border-cyan-500 p-2 items-center text-sm gap-3">
-          <div className="flex w-full items-center justify-center p-2">
-            <Calendar mode="single" className="rounded-md border w-auto h-auto" selected={date} onSelect={setDate} />
+        <aside className="flex flex-col w-72 h-svh border-r border-cyan-500 p-2 items-center text-sm gap-5">
+          
+          <div className="flex w-full items-center justify-center">
+            <Calendar mode="single" className="border w-auto h-auto shadow-lg shadow-primary/20 rounded-4xl" selected={date} onSelect={setDate} />
           </div>
-          <div className="flex justify-center items-center bg-cyan-500 w-64 text-center p-3 rounded-2xl">Gerenciar Funcionarios</div>
-          <div className="flex flex-row-reverse w-64 items-center justify-between py-2 px-3">
-            {' '}
-            <Input type="text" placeholder="Buscar Funcionario" value={funcionario} onChange={(e) => setFuncionario(e.target.value)} />{' '}
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute mr-2" />{' '}
+          
+          <div className='flex w-full justify-center items-center'>
+            <Button onClick={handleClickFuncionario} className="">Gerenciar Funcionarios</Button>
           </div>
-          <div className="flex flex-col w-full gap-1 border rounded-md py-6">
+          
+          <div className="flex w-64 mt-4 items-center py-2 px-3 space-x-1">
+            <Input type="text" placeholder="Buscar Funcionario" value={funcionario} onChange={(e) => setFuncionario(e.target.value)} />
+            <Button onClick={handleClick}><FontAwesomeIcon icon={faMagnifyingGlass} /></Button>
+          </div>
+          
+          <div className="flex flex-col w-full gap-1 border rounded-md py-6 overflow-auto">
             <ul className="border-r border-gray-400">
-              <li className="mt-2 border-b p-2 hover:bg-zinc-900">Funcionario 1</li>
-              <li className="mt-2 border-b p-2 hover:bg-zinc-900">Funcionario 2</li>
-              <li className="mt-2 border-b p-2 hover:bg-zinc-900">Funcionario 3</li>
-              <li className="mt-2 border-b p-2 hover:bg-zinc-900">Funcionario 4</li>
+              {
+                gerarFuncionarios().map((i) => {
+                  return(
+                    <li key={i.id} className="mt-2 ml-2 border-b p-2 hover:bg-secondary">{i.nome}</li>
+                  )
+                })
+              }
             </ul>
           </div>
+
         </aside>
         <main className="flex flex-col flex-1 justify-center items-center">{props.children}</main>
       </div>
