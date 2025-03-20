f0432c86e0216e26db1000518d6b98782230e742
Eliabe Gai <eliabegai@gmail.com>
2025-03-20 12:51:51 -0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://195.35.40.120:3000/:path*'
+      }
+    ]
+  },
   eslint: {
     ignoreDuringBuilds: true
   },
